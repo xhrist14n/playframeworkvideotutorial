26527125a99511d613edc2b08c93c999f279a652
khrist14n <khrist14n@gmail.com>
2017-11-03 10:28:01 -0500

    models ...

---
+++ P06Models/app/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import play.mvc.*;
+
+import views.html.*;
+
+import models.*;
+
+import java.util.*;
+
+import play.libs.Json;
+import com.fasterxml.jackson.databind.JsonNode;
+
+
+
+public class HomeController extends Controller {
+
+    public Result index() {
+        List<Person> persons = Person.find.all();
+        String data = Json.stringify(Json.toJson(persons));
+        System.out.println(data);
+        return ok(index.render(data));
+    }
+
+}
